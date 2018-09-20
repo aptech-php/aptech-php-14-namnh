5a8dd11f00914e8da5c8f6cd6eeb16e9bda14edc
[name] <namnh.website@gmail.com>
2018-09-20 21:05:36 +0700

    updated session 5

---
+++ php/2018-09-18/blog/app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function demo()
+    {
+        $user = User::all();
+        return view('welcome', ['user' => $user]);
+    }
+}
